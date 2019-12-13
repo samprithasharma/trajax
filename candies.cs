d05f3749b93001b3b1474ff185e1054c818e9f64
samprithasharma <47626107+samprithasharma@users.noreply.github.com>
2019-12-12 20:44:45 -0800

    Add files via upload

---
+++ candies.cs
@@ -0,0 +1,29 @@
+body {
+  font-family: Helvetica;
+  background-image: url(candies.jfif);
+}
+
+.header {
+  text-align: center;
+  background-color: rgba(255,255,255,0.9);
+  padding: 10px;
+  width: 70%;
+  margin: 0px auto;
+}
+.candies {
+  margin: 0px auto;
+  padding: 0px;
+  width: 70%;
+  list-style: none;
+}
+
+.item {
+  display: inline-block;
+  width: 20%;
+  background-color: rgba(255,255,255,0.9);
+  border-radius: 3px;
+  padding: 50px 0px;
+  text-align: center;
+  margin: 20px;
+}
+
